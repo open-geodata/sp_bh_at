763c3141c3ccbf7affbb2c4f86bf7138ce28e683
Michel Metran <michelmetran@gmail.com>
2022-10-19 13:22:47 -0300

    Ajustes

--- scripts/paths.py
+++ scripts/paths.py
@@ -7,10 +7,24 @@
 from pathlib import Path
 
 
-project_path = Path(__file__).parents[1]
-project_path = project_path / 'sp_bh_at'
 
-data_path = project_path / 'data'
+# Project Path
+project_path = Path(__file__).parents[1]
+
+scrapy_path = project_path / 'scrapy'
+scrapy_path.mkdir(exist_ok=True)
+
+logs_path = scrapy_path / 'logs'
+logs_path.mkdir(exist_ok=True)
+
+adds_path = scrapy_path / 'adds'
+adds_path.mkdir(exist_ok=True)
+
+
+# Package Path
+package_path = project_path / 'sp_bh_at'
+
+data_path = package_path / 'data'
 data_path.mkdir(exist_ok=True)
 
 input_path = data_path / 'input'
@@ -28,14 +42,6 @@ output_path_tab.mkdir(exist_ok=True)
 output_path_map = output_path / 'map'
 output_path_map.mkdir(exist_ok=True)
 
-scrapy_path = project_path / 'scrapy'
-scrapy_path.mkdir(exist_ok=True)
-
-logs_path = scrapy_path / 'logs'
-logs_path.mkdir(exist_ok=True)
-
-adds_path = scrapy_path / 'adds'
-adds_path.mkdir(exist_ok=True)
 
 if __name__ == '__main__':
     print(project_path)
