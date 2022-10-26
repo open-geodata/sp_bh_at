6835bb37a70ee3aa28987bd6e4497d7ff27217a7
Michel Metran <michelmetran@gmail.com>
2022-10-26 19:01:40 -0300

    Ajustes

---
+++ scripts/lixo.py
@@ -0,0 +1,25 @@
+
+    # for root, dirs, files in os.walk(output_path, topdown=False):
+    #     for file in files:
+    #         try:
+    #             shutil.move(root / file, output_path)
+    #         except OSError:
+    #             pass
+
+
+# rar_filepath = Path('c:/Users/michelsilva/Codes/sp_bh_at/sp_bh_at/data/input/brutos/asd.rar')
+# rar_filepath
+
+#unrar_files(rar_filepath, temp_path)
+
+
+    #temp_path.unlink()
+
+
+    #shutil.rmtree()
+
+
+    # rar_filepath
+# #unrar_files(rar_filepath, output_path)
+# with RarFile(rar_filepath) as rarObj:
+#     rarObj.extractall(output_path)
